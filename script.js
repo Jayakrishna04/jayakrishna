0e2e153b39c691f5c7ca9cd7d743f9a41e9854fd
Jayakrishna04 <140816644+Jayakrishna04@users.noreply.github.com>
2024-02-09 08:43:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 */
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: `${process.env.GITHUBSECRETAPIKEY}`,
   authDomain: `${process.env.GITHUBSECRETAUTHDOMAIN}`,
   projectId: `${process.env.GITHUBSECRETPROJECTID}`,
@@ -35,7 +35,17 @@ const firebaseConfig = {
   messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
   appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
   measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
+};*/
+const firebaseConfig = {
+  apiKey: process.env.GITHUBSECRET_API_KEYS,
+  authDomain: process.env.GITHUBSECRET_AUTH_DOMAIN,
+  projectId: process.env.GITHUBSECRET_PROJECT_ID,
+  storageBucket: process.env.REACTAPP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 
 
